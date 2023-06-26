cdb68e936e50424f3d7787d7d5adbb0f9a24287c
sarah-zhan <yizh.contact@gmail.com>
2023-06-25 18:01:39 -0700

    make response appears in the text box

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import { process } from './env';
+import { process } from '/env';
+import { Configuration, OpenAIApi } from 'openai';
 
 const setupTextarea = document.getElementById('setup-textarea');
 const setupInputContainer = document.getElementById('setup-input-container');
 const movieBossText = document.getElementById('movie-boss-text');
-const url = 'https://api.openai.com/v1/chat/completions';
-const apiKey = process.env.OPENAI_API_KEY;
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 
 document.getElementById('send-btn').addEventListener('click', () => {
 	if (setupTextarea.value) {
@@ -14,20 +19,12 @@ document.getElementById('send-btn').addEventListener('click', () => {
 	fetchAPI();
 });
 
-const fetchAPI = () => {
-	fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${apiKey}`,
-		},
-		body: JSON.stringify({
-			prompt: setupTextarea.value,
-			model: 'text-davinci-003',
-		}),
-	})
-		.then(response => response.json())
-		.then(data => {
-			movieBossText.innerText = data.choices[0].text;
-		});
+const fetchAPI = async () => {
+	const response = await openai.createCompletion({
+		model: 'text-davinci-003',
+		prompt: 'Sound enthusiastic in five words or less.'
+	});
+	movieBossText.innerText = response.data.choices[0].text.trim();
 };
+
+
