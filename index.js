d3dccb845698ef4af1f810724b54fa7ec6f62a6b
sarah-zhan <yizh.contact@gmail.com>
2023-06-26 16:27:33 -0700

    add examples for AI

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { process } from '/env';
 import { Configuration, OpenAIApi } from 'openai';
 
-const setupTextarea = document.getElementById('setup-textarea');
 const setupInputContainer = document.getElementById('setup-input-container');
 const movieBossText = document.getElementById('movie-boss-text');
 
@@ -12,19 +11,56 @@ const openai = new OpenAIApi(configuration);
 
 
 document.getElementById('send-btn').addEventListener('click', () => {
+	const setupTextarea = document.getElementById('setup-textarea');
 	if (setupTextarea.value) {
+		const userInput = setupTextarea.value;
 		setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
 		movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
+		fetchAPI(userInput);
+		// fetchSynopsis(userInput);
 	}
-	fetchAPI();
+
+
 });
 
-const fetchAPI = async () => {
+const fetchAPI = async (input) => {
 	const response = await openai.createCompletion({
 		model: 'text-davinci-003',
-		prompt: textarea.value
+		prompt: `Generate a 20 words message to enthusiastically sounds interesting and that you need some minutes to think about it.
+		###
+		input: A father looks for his son, who was adducted when it was 3, for 20 years in China.
+		message: I'll need to think about that. It is a happy and sad story and it uncovers a dark industry of selling children in China.
+		###
+		input: A mother chooses to stay with her brutal husband to protect her two children.
+		message: That is a sad story, but it is worthy of showing the real world of the life of this woman. Give me a few moments to think!
+		###
+		input: ${input}
+		message:
+		`,
+		max_tokens: 60,
 	});
 	movieBossText.innerText = response.data.choices[0].text.trim();
+	console.log(response)
 };
+
+const fetchSynopsis = async (input) => {
+	const response = await openai.createCompletion({
+		model: 'text-davinci-003',
+		prompt: `Generate an engaging, professional and marketable movie synopsis based on the input.
+		###
+		input: Slow-witted Forrest Gump uses his optimism to beat all the odds in life.
+
+		synopsis: Slow-witted Forrest Gump (Tom Hanks) has never thought of himself as disadvantaged, and thanks to his supportive mother (Sally Field), he leads anything but a restricted life. Whether dominating on the gridiron as a college football star, fighting in Vietnam or captaining a shrimp boat, Forrest inspires people with his childlike optimism. But one person Forrest cares about most may be the most difficult to save -- his childhood love, the sweet but troubled Jenny (Robin Wright).
+		###
+		input: ${input}
+		synopsis:
+		`,
+		max_tokens: 700,
+	});
+	// document.getElementsByClassName('output-container').style.display = 'block';
+	document.getElementById('output-text').innerText =
+		response.data.choices[0].text.trim();
+	console.log(response);
+}
 
 
